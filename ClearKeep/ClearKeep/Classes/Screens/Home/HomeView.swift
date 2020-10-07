f1bbca585e6b8e12ed0e882c68f45cad4b4dec9a
TiemLuong <tiem.luong@vmodev.com>
2020-10-07 13:54:36 +0700

    init source

---
+++ ClearKeep/ClearKeep/Classes/Screens/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  ClearKeep
+//
+//  Created by LuongTiem on 10/7/20.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("ClearKeep")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
