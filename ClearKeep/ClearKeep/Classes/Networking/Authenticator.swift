a1430bf41eb3dcf23550d2faa61a4b3e5c11c95d
TiemLuong <tiem.luong@vmodev.com>
2020-10-22 17:37:16 +0700

    feat: add service + networking

--- ClearKeep/ClearKeep/Classes/Networking/Authenticator.swift
+++ ClearKeep/ClearKeep/Classes/Networking/Authenticator.swift
@@ -8,42 +8,118 @@ import GRPC
 
 class Authenticator {
     
-    private let client: GRPCClient
+    private let client: Signalc_SignalKeyDistributionClient
     
+    var recipientStore: Signalc_SignalKeysUserResponse?
     
-    init(_ client: GRPCClient) {
+    var recipientID: String = ""
+    
+    var clientStore: CKClientStore!
+    
+    init(_ client: Signalc_SignalKeyDistributionClient) {
         self.client = client
-
     }
     
+    func loggedIn() -> Bool {
+        
+        if clientStore == nil {
+            return false
+        }
+        
+        return true
+    }
     
-    private func authenticate(signalAddess: SignalAddress = SignalAddress(identifier: "bob", deviceId: 1),
-                      bundleStore: CKBundleStore = CKBundleStore(),
-                      _ completion: @escaping (Bool, Error?) -> Void,
-                      submit: @escaping (Signalc_SignalRegisterKeysRequest, CallOptions?)
-                        -> UnaryCall<Signalc_SignalRegisterKeysRequest, Signalc_SignalKeysUserResponse>) {
+    // call register
+    private func authenticate(bundleStore: CKClientStore,
+                              _ completion: @escaping (Bool, Error?) -> Void,
+                              submit: @escaping (Signalc_SignalRegisterKeysRequest, CallOptions?)
+                                -> UnaryCall<Signalc_SignalRegisterKeysRequest, Signalc_BaseResponse>) {
         
         
         let request: Signalc_SignalRegisterKeysRequest = .with {
-            $0.clientID = signalAddess.identifier
-            $0.deviceID = Int32(signalAddess.deviceId)
-            $0.identityKeyPublic = try! bundleStore.identityKeyStore.getIdentityKeyPublicData()
-            $0.registrationID = Int32(bundleStore.preKeyStore.lastId)
-            $0.preKey = try! bundleStore.preKeyStore.preKey(for: bundleStore.preKeyStore.lastId)
-            $0.signedPreKeyID = Int32(bundleStore.signedPreKeyStore.lastId)
-            $0.signedPreKey = try! bundleStore.signedPreKeyStore.signedPreKey(for: bundleStore.signedPreKeyStore.lastId)
+            $0.clientID = bundleStore.address.name
+            $0.deviceID = bundleStore.address.deviceId
+            $0.registrationID = bundleStore.localRegistrationId
+            $0.identityKeyPublic = bundleStore.identityKeyPair.publicKey
+            $0.preKeyID = Int32(bundleStore.preKey1.preKeyId)
+            $0.preKey = bundleStore.preKey1.serializedData()!
+            $0.signedPreKeyID = Int32(bundleStore.signedPreKey.preKeyId)
+            $0.signedPreKey = bundleStore.signedPreKey.serializedData()!
+            $0.signedPreKeySignature = bundleStore.signedPreKey.signature
         }
         
         submit(request, nil).response.whenComplete { (result) in
             
-            print(result)
-            self.authenticated (completion)
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let response):
+                    print(response)
+                    completion(true, nil)
+                case .failure(_):
+                    self.authenticated(cliendID: "") { (_, _, _) in
+                        
+                    }
+                }
+            }
         }
     }
     
     
-    func authenticated(_ completion: @escaping (Bool, Error?) -> Void) {
+    
+    private func authenticated(cliendID: String,
+                               _ completion: @escaping (Bool, Error?, Signalc_SignalKeysUserResponse?) -> Void) {
+        
+        if recipientID.isEmpty || recipientStore == nil {
+            Backend.shared.authenticated(completion)
+        }
+    
+    }
+    
+    
+    private func nauthenticate(_ completion: @escaping (Bool, Error?, Signalc_SignalKeysUserResponse?) -> Void) {
+        print("auth failed")
+        recipientID = ""
+        completion(false, nil, nil)
+    }
+    
+    
+    private func login(_ clientID: String,
+               _ completion: @escaping (Bool, Error?, Signalc_SignalKeysUserResponse?) -> Void,
+               submit: @escaping (Signalc_SignalKeysUserRequest, CallOptions?)
+                -> UnaryCall<Signalc_SignalKeysUserRequest, Signalc_SignalKeysUserResponse>) {
+        
+        let request: Signalc_SignalKeysUserRequest = .with {
+            $0.clientID = clientID
+        }
+        
+        submit(request, nil).response.whenComplete { (result) in
 
-      
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let response):
+                    self.authenticated(cliendID: response.clientID,
+                                       completion)
+                completion(true, nil, response)
+                case .failure(_):
+                    self.nauthenticate(completion)
+                }
+            }
+        }
+    }
+    
+}
+
+extension Authenticator {
+    
+    func register(bundleStore: CKClientStore, completion: @escaping (Bool, Error?) -> Void) {
+        authenticate(bundleStore: bundleStore, completion, submit: client.registerBundleKey)
+    }
+    
+}
+
+extension Authenticator {
+    
+    func login(_ clientID: String, _ completion: @escaping (Bool, Error?, Signalc_SignalKeysUserResponse?) -> Void) {
+        login(clientID, completion, submit: client.getKeyBundleByUserId)
     }
 }
--- ClearKeep/ClearKeep/Classes/Networking/Backend.swift
+++ ClearKeep/ClearKeep/Classes/Networking/Backend.swift
@@ -1,11 +1,11 @@
 
 import Foundation
 import Combine
-import CryptoKit
 
 import SwiftProtobuf
 import NIO
 import GRPC
+import SignalProtocolObjC
 
 class Backend: ObservableObject {
     
@@ -13,11 +13,19 @@ class Backend: ObservableObject {
     
     private let group: MultiThreadedEventLoopGroup
     
-    private let client: GRPCClient
+    private let client: Signalc_SignalKeyDistributionClient
     
     private let connection: ClientConnection
     
     var authenticator: Authenticator
+    
+    var signalService: SignalService?
+    
+    private var queueHandShake: [String: String] = [:]
+    
+    
+    @Published var messages: [PostModel] = []
+    @Published var rooms = [RoomModel]()
     
     
     init(host: String = "localhost", port: Int = 50051) {
@@ -27,9 +35,12 @@ class Backend: ObservableObject {
         
         connection = ClientConnection(configuration: configuration)
         
-        client = GRPC.AnyServiceClient(channel: connection)
+        client = Signalc_SignalKeyDistributionClient(channel: connection)
         
         authenticator = Authenticator(client)
+        
+        signalService = SignalService(client)
+        
     }
     
     deinit {
@@ -50,8 +61,93 @@ class Backend: ObservableObject {
     }
     
     
-    func authenticated(signAddresss: SignalAddress, bundleStore: CKBundleStore, _ completion: @escaping (Bool, Error?) -> Void) {
+    func authenticated(_ completion: @escaping (Bool, Error?, Signalc_SignalKeysUserResponse?) -> Void) {
        
+        signalService?.listen(heard: heard)
+        
+        signalService?.subscribe(clientID: authenticator.clientStore.address.name, { (result, error, response) in
+            
+        })
+    }
+    
+    
+    private func heard(_ clienID: String, publication: Signalc_Publication) {
+        
+        if authenticator.clientStore == nil {
+            return
+        }
+        
+        let remoteAddress = SignalAddress(name: publication.senderID, deviceId: authenticator.recipientStore!.deviceID)
+        
+        do {
+            let messageData = try authenticator.clientStore.decrypt(remoteAddress: remoteAddress, cipherData: publication.message)
+            
+            let postModel = PostModel(from: publication.senderID, message: messageData)
+            
+            self.messages.append(postModel)
+            
+        } catch {
+            print(error)
+        }
         
     }
+    
+    
+    func send(_ message: String,
+              to recipient: String,
+              _ completion: @escaping (Bool, Error?) -> Void) {
+        
+        guard let recipientStore = authenticator.recipientStore else {
+            completion(false, nil)
+            return
+        }
+        
+        do {
+            let remoteAddress = SignalAddress(name: recipientStore.clientID, deviceId: recipientStore.deviceID)
+            
+            let cipherText = try authenticator.clientStore.encrypt(remoteAddress: remoteAddress,
+                                                                   recipientStore: authenticator.recipientStore!,
+                                                                   message: message)
+            
+            let request: Signalc_PublishRequest = .with {
+                $0.receiveID = recipient
+                $0.senderID = authenticator.clientStore.address.name
+                $0.message = cipherText.data
+            }
+            
+            try self.send(request, to: recipient)
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    private func send(_ request: Signalc_PublishRequest,
+                        to recipient: String,
+                        _ completion: ((Bool, Error?) -> Void)? = nil) throws {
+        
+        client.publish(request).response.whenComplete { result in
+            switch result {
+            case .success(let response):
+                print(response)
+                completion?(false, nil)
+            case .failure(let error):
+                completion?(false, error)
+            }
+        }
+    }
+}
+
+
+struct PostModel: Identifiable {
+    let id: Int8 = 11
+    var newID = UUID().uuidString
+    var from: String
+    var message: Data
+}
+
+struct RoomModel: Identifiable, Hashable {
+    var id: String
+
 }
