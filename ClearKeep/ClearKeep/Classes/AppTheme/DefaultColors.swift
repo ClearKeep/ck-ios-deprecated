5ca286a75d4cbb15e03b2bea364b69ef5ea3e3f1
hiep <luongtsu@gmail.com>
2021-04-13 15:07:42 +0700

    update color using Color Literal

--- ClearKeep/ClearKeep/Classes/AppTheme/DefaultColors.swift
+++ ClearKeep/ClearKeep/Classes/AppTheme/DefaultColors.swift
@@ -52,47 +52,47 @@ struct DefaultColors: AppColors {
     var label: UIColor
     
     init(
-        primary: UIColor = UIColor(hex: "#6267FB"),
-        primaryDark: UIColor = UIColor(hex: "#363BD0"),
-        primaryLight: UIColor = UIColor(hex: "#898DFF"),
+        primary: UIColor = #colorLiteral(red: 0.3835987449, green: 0.4038437307, blue: 0.9832308888, alpha: 1),// UIColor(hex: "#6267FB"),
+        primaryDark: UIColor = #colorLiteral(red: 0.2133642435, green: 0.2326667011, blue: 0.8172033429, alpha: 1),// UIColor(hex: "#363BD0"),
+        primaryLight: UIColor = #colorLiteral(red: 0.5383666158, green: 0.5527175069, blue: 1, alpha: 1),// UIColor(hex: "#898DFF"),
         
-        secondary: UIColor = UIColor(hex: "#E06464"),
-        secondaryDark: UIColor = UIColor(hex: "#D42B2B"),
-        secondaryLight: UIColor = UIColor(hex: "#E99191"),
+        secondary: UIColor = #colorLiteral(red: 0.8788725138, green: 0.3924512863, blue: 0.3940677047, alpha: 1),// UIColor(hex: "#E06464"),
+        secondaryDark: UIColor = #colorLiteral(red: 0.8327288032, green: 0.1624940932, blue: 0.1665982008, alpha: 1),// UIColor(hex: "#D42B2B"),
+        secondaryLight: UIColor = #colorLiteral(red: 0.9126839042, green: 0.5627285242, blue: 0.5630574226, alpha: 1),// UIColor(hex: "#E99191"),
         
-        error: UIColor = UIColor(hex: "#ED2E7E"),
-        errorDark: UIColor = UIColor(hex: "#C30052"),
-        errorLight: UIColor = UIColor(hex: "#FFE8F1"),
+        error: UIColor = #colorLiteral(red: 0.9275476336, green: 0.1823432744, blue: 0.4958347678, alpha: 1),// UIColor(hex: "#ED2E7E"),
+        errorDark: UIColor = #colorLiteral(red: 0.7654986978, green: 0.0001034540401, blue: 0.3202075958, alpha: 1),// UIColor(hex: "#C30052"),
+        errorLight: UIColor = #colorLiteral(red: 0.9980357289, green: 0.9088183045, blue: 0.9453745484, alpha: 1),// UIColor(hex: "#FFE8F1"),
         
-        success: UIColor = UIColor(hex: "#00BA88"),
-        successDark: UIColor = UIColor(hex: "#00966D"),
-        successLight: UIColor = UIColor(hex: "#DBFFF5"),
+        success: UIColor = #colorLiteral(red: 0, green: 0.7297249436, blue: 0.5331287384, alpha: 1),// UIColor(hex: "#00BA88"),
+        successDark: UIColor = #colorLiteral(red: 0.001809648122, green: 0.5877657533, blue: 0.4280515313, alpha: 1),// UIColor(hex: "#00966D"),
+        successLight: UIColor = #colorLiteral(red: 0.8587369323, green: 1, blue: 0.9605641961, alpha: 1),// UIColor(hex: "#DBFFF5"),
         
-        warning: UIColor = UIColor(hex: "#F4B740"),
-        warningDark: UIColor = UIColor(hex: "#946200"),
-        warningLight: UIColor = UIColor(hex: "#FFD789"),
+        warning: UIColor = #colorLiteral(red: 0.9587331414, green: 0.7176125646, blue: 0.250107795, alpha: 1),// UIColor(hex: "#F4B740"),
+        warningDark: UIColor = #colorLiteral(red: 0.5808352232, green: 0.3825485706, blue: 0, alpha: 1),// UIColor(hex: "#946200"),
+        warningLight: UIColor = #colorLiteral(red: 1, green: 0.8426898122, blue: 0.5372179747, alpha: 1),// UIColor(hex: "#FFD789"),
         
-        gradientPrimaryDark: UIColor = UIColor(hex: "#7773F3"),
-        gradientPrimaryLight: UIColor = UIColor(hex: "#8ABFF3"),
+        gradientPrimaryDark: UIColor = #colorLiteral(red: 0.4681581855, green: 0.4513198733, blue: 0.9543935657, alpha: 1),// UIColor(hex: "#7773F3"),
+        gradientPrimaryLight: UIColor = #colorLiteral(red: 0.5410716534, green: 0.7494730949, blue: 0.953292191, alpha: 1),// UIColor(hex: "#8ABFF3"),
         
-        gradientSecondaryDark: UIColor = UIColor(hex: "#363BD0"),
-        gradientSecondaryLight: UIColor = UIColor(hex: "#64A1E0"),
+        gradientSecondaryDark: UIColor = #colorLiteral(red: 0.2133642435, green: 0.2326667011, blue: 0.8172033429, alpha: 1),// UIColor(hex: "#363BD0"),
+        gradientSecondaryLight: UIColor = #colorLiteral(red: 0.3936348557, green: 0.6305711269, blue: 0.8798473477, alpha: 1),// UIColor(hex: "#64A1E0"),
         
-        gradientAccentDark: UIColor = UIColor(hex: "#E06464"),
-        gradientAccentLight: UIColor = UIColor(hex: "#E99191"),
+        gradientAccentDark: UIColor = #colorLiteral(red: 0.8788725138, green: 0.3924512863, blue: 0.3940677047, alpha: 1),// UIColor(hex: "#E06464"),
+        gradientAccentLight: UIColor = #colorLiteral(red: 0.9126275778, green: 0.566683352, blue: 0.5670747161, alpha: 1),// UIColor(hex: "#E99191"),
         
-        black: UIColor = UIColor(hex: "#000000"),
-        gray1: UIColor = UIColor(hex: "#4E4B66"),
-        gray2: UIColor = UIColor(hex: "#6E7191"),
-        gray3: UIColor = UIColor(hex: "#A0A3BD"),
-        gray4: UIColor = UIColor(hex: "#D9DBE9"),
-        gray5: UIColor = UIColor(hex: "#EFF0F6"),
-        background: UIColor = UIColor(hex: "#F7F7FC"),
-        offWhite: UIColor = UIColor(hex: "#FCFCFC"),
+        black: UIColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1),// UIColor(hex: "#000000"),
+        gray1: UIColor = #colorLiteral(red: 0.3056983948, green: 0.2958287597, blue: 0.399384588, alpha: 1),// UIColor(hex: "#4E4B66"),
+        gray2: UIColor = #colorLiteral(red: 0.4304504395, green: 0.4445848465, blue: 0.5689668655, alpha: 1),// UIColor(hex: "#6E7191"),
+        gray3: UIColor = #colorLiteral(red: 0.626722157, green: 0.6372935176, blue: 0.7409598827, alpha: 1),// UIColor(hex: "#A0A3BD"),
+        gray4: UIColor = #colorLiteral(red: 0.8505116105, green: 0.8579232097, blue: 0.9117991328, alpha: 1),// UIColor(hex: "#D9DBE9"),
+        gray5: UIColor = #colorLiteral(red: 0.9369740486, green: 0.9408063293, blue: 0.9655880332, alpha: 1),// UIColor(hex: "#EFF0F6"),
+        background: UIColor = #colorLiteral(red: 0.9684610963, green: 0.968318522, blue: 0.9889139533, alpha: 1),// UIColor(hex: "#F7F7FC"),
+        offWhite: UIColor = #colorLiteral(red: 0.9881282449, green: 0.9882970452, blue: 0.988117516, alpha: 1),// UIColor(hex: "#FCFCFC"),
         
-        body: UIColor = UIColor(hex: "#4E4B66"),
-        titleActive: UIColor = UIColor(hex: "#14142B"),
-        label: UIColor = UIColor(hex: "#6E7191")
+        body: UIColor = #colorLiteral(red: 0.3056983948, green: 0.2958287597, blue: 0.399384588, alpha: 1),// UIColor(hex: "#4E4B66"),
+        titleActive: UIColor = #colorLiteral(red: 0.0780204162, green: 0.07602141052, blue: 0.1672900021, alpha: 1),// UIColor(hex: "#14142B"),
+        label: UIColor = #colorLiteral(red: 0.4304504395, green: 0.4445848465, blue: 0.5689668655, alpha: 1) // UIColor(hex: "#6E7191")
     ) {
         self.primary = primary
         self.primaryDark = primaryDark
