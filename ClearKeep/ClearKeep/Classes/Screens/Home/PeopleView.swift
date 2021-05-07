b49b120dbfb1f045a2373e5592e66b7a79647b8b
NamNH <>
2021-05-07 10:29:19 +0700

    CLK32-358: Refactor navigation of PeopleView

--- ClearKeep/ClearKeep/Classes/Screens/Home/PeopleView.swift
+++ ClearKeep/ClearKeep/Classes/Screens/Home/PeopleView.swift
@@ -17,72 +17,52 @@ struct PeopleView: View {
     
     @State private var peoples : [People] = []
     @State var hudVisible : Bool = false
-        
+    
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-
+    
     
     var body: some View {
         GeometryReader { geometry in
-            VStack(alignment: .leading , spacing: 0) {
-                Spacer()
-                    .grandientBackground()
-                    .frame(width: UIScreen.main.bounds.width, height: 60)
+            VStack(alignment: .leading) {
+                Text("User in this Channel")
+                    .font(AppTheme.fonts.textMedium.font)
+                    .foregroundColor(AppTheme.colors.gray2.color)
+                    .padding([.top , .bottom] , 16)
                 
-                VStack(alignment: .leading) {
-                    Button {
-                        presentationMode.wrappedValue.dismiss()
-                        
-                    } label: {
-                        Image("ic_close")
-                            .frame(width: 24, height: 24)
-                            .foregroundColor(AppTheme.colors.gray1.color)
-                    }
-                    .padding(.top, 29)
-                    
-                    Text("New Message")
-                        .font(AppTheme.fonts.linkLarge.font)
-                        .foregroundColor(AppTheme.colors.black.color)
-                        .padding(.top, 23)
-                    
-                    SearchBar(text: $searchText) { (changed) in
-                        if changed {
-                        } else {
-                            self.searchUser(searchText)
-                        }
-                    }
-                    
-                    Text("User in this Channel")
-                        .font(AppTheme.fonts.textMedium.font)
-                        .foregroundColor(AppTheme.colors.gray2.color)
-                        .padding([.top , .bottom] , 16)
-                    
-                    Group {
-                        ScrollView(.vertical, showsIndicators: false, content: {
-                            HStack {
-                                VStack(alignment:.leading , spacing: 16) {
-                                    ForEach(self.peoples , id: \.id) { user in
-                                        NavigationLink(destination:  MessageChatView(clientId: user.id, groupID: 0, userName: user.userName)
-                                                        .environmentObject(groupRealms)
-                                                        .environmentObject(messsagesRealms)){
-                                            ContactView(people: user)
-                                        }
+                Group {
+                    ScrollView(.vertical, showsIndicators: false, content: {
+                        HStack {
+                            VStack(alignment:.leading , spacing: 16) {
+                                ForEach(self.peoples , id: \.id) { user in
+                                    NavigationLink(destination:  MessageChatView(clientId: user.id, groupID: 0, userName: user.userName)
+                                                    .environmentObject(groupRealms)
+                                                    .environmentObject(messsagesRealms)){
+                                        ContactView(people: user)
                                     }
                                 }
-                                
-                                Spacer()
                             }
-                            .frame(width: geometry.size.width - 32)
-                        })
-                    }
-                    
-                    
+                            
+                            Spacer()
+                        }
+                        .frame(width: geometry.size.width - 32)
+                    })
                 }
-                .padding([.trailing , .leading , .bottom] , 16)
+                
+                
             }
+            .padding([.trailing , .leading , .bottom] , 16)
+            .applyNavigationBarStyle(title: "New Message", leftBarItems: {
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Image("ic_close")
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(AppTheme.colors.gray1.color)
+                }
+            }, rightBarItems: {
+                Spacer()
+            })
         }
-        .navigationBarTitle(Text(""), displayMode: .inline)
-        .navigationBarHidden(true)
-        .edgesIgnoringSafeArea(.top)
         .onAppear(){
             self.getListUser()
         }
