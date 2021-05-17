3e465c3aa8969f59cac717c67b5b7442343cfc77
NamNH <unknown>
2021-05-17 11:31:06 +0700

    CLK32-440: Add new chatview

---
+++ ClearKeep/ClearKeep/Classes/Screens/MessengerView/MessageCommonView.swift
@@ -0,0 +1,20 @@
+//
+//  MessageCommonView.swift
+//  ClearKeep
+//
+//  Created by Nguyễn Nam on 5/17/21.
+//
+
+import SwiftUI
+
+struct MessageCommonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessageCommonView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageCommonView()
+    }
+}
